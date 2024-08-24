9d095658bae5a7d7b8b4216099848f2472d19a43
Tapraj Pathak <taprajpathak111@gmail.com>
2024-08-24 17:15:34 +0545

    client code changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,18 @@
 import React from 'react'
+import { useEffect,useState } from 'react'
+import axios from 'axios'
 
 function App() {
+
+  const [users, setUsers] = useState([]);
+  // Fetch data when component mounts
+  //Example to get api from own code
+  useEffect(async () =>{
+    const response = await axios.get("/api/users");
+    setUsers(response.data);
+    console.log(users);
+  },[]);
+
   return (
     <div className='bg-slate-500'>App</div>
   )
